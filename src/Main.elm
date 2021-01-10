f2cbc2a51c6d3a8c73bfd13cc3ec6284aa6f6653
geofflangenderfer <geoff.langenderfer@gmail.com>
2021-01-09 19:28:05 -0500

    Chuck Norris h1 working

---
+++ src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (..)
+
+import Browser 
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+
+main: Program () Model Msg
+main = 
+    Browser.element
+        { init = init 
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
+type alias Model =
+    { quote: String
+    }
+
+init : () -> (Model, Cmd Msg)
+init _ = 
+    (Model "", Cmd.none)
+
+type Msg = 
+    GetQuote
+
+update: Msg -> Model -> (Model, Cmd Msg) 
+update msg model =
+    case msg of 
+        GetQuote ->
+            ( { model | quote = model.quote ++ "A quote! " }, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Chuck Norris!"]
+        , text model.quote
+        ]
