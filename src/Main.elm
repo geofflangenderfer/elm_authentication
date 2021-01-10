33e798e77d77dbbd782f8bf10d1ef7050d3b5697
geofflangenderfer <geoff.langenderfer@gmail.com>
2021-01-09 20:02:17 -0500

    successfully getting/displaying random quote

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Browser 
+import Http exposing (..)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
@@ -22,15 +23,25 @@ init : () -> (Model, Cmd Msg)
 init _ = 
     (Model "", Cmd.none)
 
-type Msg = 
-    GetQuote
+type Msg 
+    = GetQuote
+    | QuoteReceived (Result Http.Error String)
 
 update: Msg -> Model -> (Model, Cmd Msg) 
 update msg model =
     case msg of 
         GetQuote ->
-            ( { model | quote = model.quote ++ "A quote! " }, Cmd.none)
+            (model, getQuote)
+        QuoteReceived (Ok result) ->
+            ( { model | quote = result }, Cmd.none)
+        QuoteReceived (Err _) ->
+            ( { model | quote = "There was an error" }, Cmd.none)
 
+getQuote =
+    Http.get
+    { url = "http://localhost:3001/api/random-quote"
+    , expect = Http.expectString QuoteReceived
+    }
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
@@ -40,5 +51,5 @@ view model =
             [ button [ class "btn btn-success", onClick GetQuote ] 
                 [ text "Get Quote" ]
             ]
-        , text model.quote
+        , blockquote [] [ text model.quote ]
         ]
